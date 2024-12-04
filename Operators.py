35444da70dd98b473f7733718f25a128cbb6c851
YOGEESH K P <yogeesh793@gmail.com>
2024-12-04 09:47:11 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,56 @@
+# Operators in Python Programming
+# comparision operator
+
+a=7
+b=16
+print(a>b)
+
+a=7
+b=16
+print(a<b)
+
+a=7
+b=16
+print(a==b)
+
+a=16
+b=16
+print(a==b)
+
+a=7
+b=16
+print(a!=b)
+
+a=16
+b=16
+print(a!=b)
+
+
+a=16
+b=16
+print(a>b)
+
+a=16
+b=16
+print(a>=b)
+
+a=16
+b=16
+print(a<=b)
+
+a=17
+b=16
+print(a<=b)
+
+# Logical Operator
+print(25>24 and 32==23)
+print(25>24 and 32==32)
+
+print(25>24 or 32==23)
+print(25>24 or 32==32)
+
+print(not 25==25)
+
+print(not True)
+
+print(not False)
